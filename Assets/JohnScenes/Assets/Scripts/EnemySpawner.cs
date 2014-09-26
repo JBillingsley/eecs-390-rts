c203ba48a7cd251c57cdbb6226d998e3b6624db7
John <jmb330@case.edu>
2014-09-26 10:16:41 -0400

    added simple enemy spawner

---
+++ Assets/JohnScenes/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+public float lastEnemyTime;
+public float enemySpawnTime;
+public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		lastEnemyTime = Time.timeSinceLevelLoad;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad - lastEnemyTime > enemySpawnTime){
+			lastEnemyTime = Time.timeSinceLevelLoad;
+			 Instantiate(enemy, transform.position, Quaternion.identity);
+		}
+	}
+}
