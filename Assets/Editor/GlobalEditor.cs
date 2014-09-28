3c836c59856fe68daac6c6529f05bfa1228de16c
abagraba <abagraba@gmail.com>
2014-09-28 12:04:57 -0400

    Should be working

--- Assets/Editor/GlobalEditor.cs
+++ Assets/Editor/GlobalEditor.cs
@@ -11,16 +11,23 @@ public class GlobalEditor : Editor {
 
 		GUI.enabled = !Application.isPlaying;
 
-		EditorGUI.indentLevel++;
-		EditorGUI.BeginChangeCheck();
+		renderTilesets ();
+		renderAnimationsets ();
 
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	private void renderTilesets(){
 		SerializedProperty list = serializedObject.FindProperty ("tilesets");
 		EditorGUILayout.PropertyField(list);
+
+		EditorGUI.indentLevel++;
 		if (list.isExpanded){
 			for (int index = 0; index < list.arraySize; index++){
 				EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(index), true);
 				Rect lr = GUILayoutUtility.GetLastRect();
-				Rect r = new Rect (lr.x, lr.y + 4 * TilesetDrawer.dy, lr.width - TilesetDrawer.texSize - lr.x - TilesetDrawer.padding, TilesetDrawer.dy);
+				float dx = EditorGUI.indentLevel * 8 + TilesetDrawer.padding;
+				Rect r = new Rect (lr.x + dx, lr.y + 4 * TilesetDrawer.dy, lr.width - TilesetDrawer.texSize - lr.x - TilesetDrawer.padding - dx, TilesetDrawer.dy);
 				if (GUI.Button (r, "Delete Tileset")) {
 					list.DeleteArrayElementAtIndex(index);
 				}
@@ -33,13 +40,37 @@ public class GlobalEditor : Editor {
 				tileSet.FindPropertyRelative("name").stringValue = "Tileset Name";
 				tileSet.serializedObject.ApplyModifiedProperties();
 			}
-
+			
 			EditorGUIUtility.LookLikeControls();
-			EditorGUI.indentLevel--;
 		}
-		serializedObject.ApplyModifiedProperties();
+		EditorGUI.indentLevel--;
 	}
 	
-	
-	
+	private void renderAnimationsets(){
+		SerializedProperty list = serializedObject.FindProperty ("animationSets");
+		EditorGUILayout.PropertyField(list);
+		
+		EditorGUI.indentLevel++;
+		if (list.isExpanded){
+			for (int index = 0; index < list.arraySize; index++){
+				EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(index), true);/*
+				Rect lr = GUILayoutUtility.GetLastRect();
+				Rect r = new Rect (lr.x, lr.y + 4 * TilesetDrawer.dy, lr.width - TilesetDrawer.texSize - lr.x - TilesetDrawer.padding, TilesetDrawer.dy);
+				if (GUI.Button (r, "Delete Tileset")) {
+					list.DeleteArrayElementAtIndex(index);
+				}*/
+			}
+			if (GUILayout.Button ("Add Animation Set")) {
+				int i = list.arraySize++;/*
+				SerializedProperty tileSet = list.GetArrayElementAtIndex(i);
+				tileSet.FindPropertyRelative("width").intValue = 1;
+				tileSet.FindPropertyRelative("height").intValue = 1;
+				tileSet.FindPropertyRelative("name").stringValue = "Tileset Name";
+				tileSet.serializedObject.ApplyModifiedProperties();*/
+			}
+			
+			EditorGUIUtility.LookLikeControls();
+		}
+		EditorGUI.indentLevel--;
+	}
 }
---
+++ Assets/FixedAttribute.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedAttribute : PropertyAttribute {
+	public FixedAttribute () {
+	}
+}
