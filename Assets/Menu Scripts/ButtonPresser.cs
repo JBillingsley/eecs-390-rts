c7089b30dfd35037b4290b357c25330ebda320e5
kleinzach <henry.eastman@gmail.com>
2014-10-01 21:14:11 -0400

    Added my menu scripts.

---
+++ Assets/Menu Scripts/ButtonPresser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPresser : MonoBehaviour {
+
+	public LayerMask buttons;
+
+	void Start(){
+		Time.timeScale = 1;
+		Time.fixedDeltaTime = 0.02F * Time.timeScale;
+	}
+
+	void OnGUI(){
+		RaycastHit2D hit;
+		Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		if(Input.GetMouseButtonDown(0)){
+			if(hit = Physics2D.GetRayIntersection(r,100f,buttons.value)){
+				(hit.collider.GetComponent(typeof(Button)) as Button).press();
+				return;
+			}
+		}
+	}
+}
---
+++ Assets/Menu Scripts/LevelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButton : Button {
+
+	public string levelName;
+
+	public override void press () {
+		Application.LoadLevel(levelName);
+	}
+}
