07bf9a5063f5240c975a0295e1d335a6c882aba2
John <jmb330@case.edu>
2014-10-04 14:27:20 -0400

    created button script so we can have pop up tooltips for things

---
+++ Assets/JohnScenes/Assets/Scripts/InGameMenuButtons.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenuButtons : MonoBehaviour {
+
+
+	public string content;
+	public float overlayHeight;
+	public float overlayWidth;
+	public float toolTipPercentLeft;
+	public float toolTipPercentDown;
+	public float screenXPosition;
+	public float screenYPosition;
+	public bool buttonMousedOver = false;
+	public Vector3 scale;
+	
+	void OnMouseOver(){
+		buttonMousedOver = true;
+	}
+	
+	void OnMouseExit(){
+		buttonMousedOver = false;
+	}
+	
+	void OnGUI(){
+		if(buttonMousedOver){
+			worldToScreenPositions();
+			GUI.TextArea(new Rect((toolTipPercentLeft * Screen.width/100), (toolTipPercentDown * Screen.height/100), (overlayWidth * scale.x) * Screen.width/100, (overlayHeight * scale.y)* Screen.height/100), content);
+		}
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		worldToScreenPositions();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void worldToScreenPositions(){
+		float currentObjectWidth = gameObject.renderer.bounds.size.x;
+		float currentObjectHeight = gameObject.renderer.bounds.size.y;
+		
+		Vector3 scale = transform.localScale;
+		
+		scale.x = overlayWidth * scale.x/currentObjectWidth;
+		scale.y = overlayHeight * scale.y/currentObjectHeight;
+		
+		this.scale = scale;
+		
+		//transform.localScale = scale;
+	}
+}
