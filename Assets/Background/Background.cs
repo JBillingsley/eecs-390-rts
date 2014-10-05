00929b0a8fc715b882a6d468b362f0b0673e6ccf
abagraba <abagraba@gmail.com>
2014-10-05 17:14:42 -0400

    Added layered background scrolling

--- Assets/Background/Background.cs
+++ Assets/Background/Background.cs
@@ -16,6 +16,6 @@ public class Background : MonoBehaviour {
 		float minHeight = Mathf.Min(Screen.width/2, Screen.height);
 		float h = Mathf.Max(camerax.orthographicSize * 2);
 		transform.localScale = new Vector2 (2*h, h);
-		transform.localPosition = new Vector3 (-h, -h/2, 10);
+		transform.localPosition = new Vector3 (-h, -h/2, transform.localPosition.z);
 	}
 }
