e03153f97a9e9121e64b04130c0b0bda90167f1e
John <jmb330@case.edu>
2014-11-03 20:47:49 -0500

    set UI to fixed position and relative scaling

--- Assets/Menu Scripts/ScreenAnchor.cs
+++ Assets/Menu Scripts/ScreenAnchor.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class ScreenAnchor : MonoBehaviour {
-	public float yOffset;
-	public float xOffset;
+	public GameObject ui;
+	public float yOffSet;
+	public float xOffSet;
 	// Update is called once per frame
 	void Update () {
-		Vector3 screenPosition = transform.position;
-		screenPosition = Camera.main.WorldToViewportPoint(screenPosition);
-		transform.position = Camera.main.ScreenToWorldPoint(screenPosition);
-		screenPosition = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z);
-		transform.position = screenPosition;
+		float camHalfHeight = Camera.main.orthographicSize;
+		float camHalfWidth = Camera.main.aspect * camHalfHeight; 
+		
+		Bounds bounds = ui.GetComponent<SpriteRenderer>().bounds;
+		
+		// Set a new vector to the top left of the scene 
+		Vector3 topLeftPosition = new Vector3(-camHalfWidth, camHalfHeight, 0) + Camera.main.transform.position; 
+		
+		// Offset it by the size of the object 
+		topLeftPosition += new Vector3(bounds.size.x - xOffSet,-bounds.size.y + yOffSet , 0);
+		
+		transform.position = topLeftPosition;        
 	}
 }
