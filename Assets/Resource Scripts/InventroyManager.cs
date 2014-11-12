9254c6e47779ae44134edf234ad17ac005595bdf
John <jmb330@case.edu>
2014-11-12 17:09:21 -0500

    creating an inventory manager, and added to the gather method of units for inventory handling

---
+++ Assets/Resource Scripts/InventroyManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventroyManager : MonoBehaviour {
+
+	private static InventroyManager _instance;
+	private int[] inventory;
+	// Use this for initialization
+	void Start () {
+		_instance = this;
+		inventory = new int[Element.length];
+	}
+	
+	public static InventroyManager instance {
+		get {
+			if(_instance == null){
+				_instance = GameObject.FindObjectOfType<InventroyManager>();
+			}
+			return _instance;
+		}
+	}
+	
+	public void addToInventory(Element item){
+		inventory[(int)item] += 1;
+	}
+	
+	public void removeFromInventory(Element item, int amount){
+		inventory[(int)item] -= amount;
+	}
+	
+	public int getCount(Element type){
+		return inventory[(int)type];
+	}
+}
