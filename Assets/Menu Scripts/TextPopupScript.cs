5ef04620d179532459a6f05d94b066f571ddf483
John <jmb330@case.edu>
2014-11-15 15:00:34 -0500

    added character icons to text boxes

---
+++ Assets/Menu Scripts/TextPopupScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextPopupScript : MonoBehaviour {
+	
+	public string textToDisplay;
+	public Texture textBox;
+	public Texture characterIcon;
+	
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(Screen.width/5, Screen.height/5, Screen.width*3/5, Screen.height*3/5), textBox);
+		// this is the second box, which is the furthest 3rd of the panel
+		GUI.DrawTexture(new Rect(Screen.width*3/5, Screen.height/5, Screen.width*1/5, Screen.height*3/5), textBox);
+		// this is for the character icon
+		GUI.DrawTexture(new Rect(Screen.width*3/5, Screen.height/5 + Screen.width/15, Screen.width/5, Screen.width/5), characterIcon);
+		GUI.Label(new Rect(Screen.width/5 + Screen.width/20, Screen.height/5 + Screen.height/20, Screen.width*3/5, Screen.height*3/5), textToDisplay);
+	}
+}
