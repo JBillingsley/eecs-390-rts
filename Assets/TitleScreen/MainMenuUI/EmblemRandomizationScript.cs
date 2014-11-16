39a042e3379fdb70094b02e0d54fe4e1f4691883
John <jmb330@case.edu>
2014-11-15 19:52:42 -0500

    made title screen more pretty

---
+++ Assets/TitleScreen/MainMenuUI/EmblemRandomizationScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmblemRandomizationScript : MonoBehaviour {
+
+	public Texture[] titles;
+	public float heightOffset;
+	public float scale;
+	public int titleToUse;
+	public float ratio;
+	
+	void Start(){
+		titleToUse = (int)Random.Range(0, titles.Length);
+		scale = scale / 100;
+	}
+	
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(Screen.width/2 - titles[titleToUse].width/2*scale, Screen.height - Screen.height/heightOffset, titles[titleToUse].width*scale, titles[titleToUse].height*scale), titles[titleToUse]);
+	}
+}
