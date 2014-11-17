19331bd33cc7a49be7a9456b7a8f548059101b19
John <jmb330@case.edu>
2014-11-17 18:47:15 -0500

    fixed inventory bar

--- Assets/Menu Scripts/InventoryPanelScript.cs
+++ Assets/Menu Scripts/InventoryPanelScript.cs
@@ -7,22 +7,24 @@ public class InventoryPanelScript : MonoBehaviour {
 	public Texture buttonTexure;
 	public Texture[] iconTexture;
 	public Button[] buttons;
-	public float panelWidth;
+	//public float panelWidth;
 	public float panelHeight;
 	public float padding;
+	private string amount;
+	public Element[] type;
 	
 	void OnGUI(){
-		panelHeight = (Screen.width/1.618f)/12;
-		panelWidth = Screen.width/1.5f;
-		GUI.DrawTexture(new Rect(((panelHeight + padding) * (iconTexture.Length)), Screen.height - panelHeight, ((panelHeight + padding) * (iconTexture.Length)) + padding, panelHeight), panelTexture);
+		panelHeight = (Screen.width/1.618f)/20;
+	//	panelWidth = Screen.width/1.5f;
+		GUI.DrawTexture(new Rect((Screen.width/2 - ((panelHeight + padding) * (iconTexture.Length))/2), Screen.height - panelHeight, ((panelHeight + padding) * (iconTexture.Length)) + padding, panelHeight), panelTexture);
 		for(int i = 0; i < iconTexture.Length; i++){
 			float buttonOffset = ((panelHeight + padding)*i) + padding;
-			if(GUI.Button(new Rect(((panelHeight + padding) * (iconTexture.Length)) + buttonOffset, Screen.height - panelHeight, panelHeight, panelHeight), iconTexture[i])){
+			if(GUI.Button(new Rect(((Screen.width/2 - ((panelHeight + padding) * (iconTexture.Length))/2)) + buttonOffset, Screen.height - panelHeight, panelHeight, panelHeight), iconTexture[i])){
 				
 			}
 			// TODO: change this to the inventory numbers
-			string content = "0";
-			GUI.Label(new Rect((((panelHeight + padding) * (iconTexture.Length)) + buttonOffset) + (3*panelHeight/4), Screen.height - panelHeight, panelHeight/4, panelHeight/2), content);
+			amount = "" + InventroyManager.instance.getCount(type[i]);
+			GUI.Label(new Rect((((Screen.width/2 - ((panelHeight + padding) * (iconTexture.Length))/2)) + buttonOffset) + (3*panelHeight/4) - 5, Screen.height - panelHeight, panelHeight/3, panelHeight/2), amount);
 		}
 	}
 }
