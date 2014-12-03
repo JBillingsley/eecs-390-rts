7b93b720c2c7e78be77a76fe1f783faca9327911
John Billingsley <jmb330@case.edu>
2014-12-03 15:06:35 -0500

    henry changes

--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class Tower : MapAdjuster {
 
 	public int height = 0;
+	public TowerType type = TowerType.WOOD;
+	
+	public enum TowerType {WOOD,CLAY,STONE};
 
 	public TileCluster towerCluster = new TileCluster();
 
 	static int AIR = 0;
-	static int TOWER = 11;
+	static int TOWER_STONE = 11;
+	static int TOWER_CLAY = 12;
+	static int TOWER_WOOD = 13;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +21,7 @@ public class Tower : MapAdjuster {
 		towerCluster.x = 1;
 		towerCluster.y = 1;
 		towerCluster.width = 4;
-		towerCluster.height = height;
+		towerCluster.height = 0;
 		towerCluster.background = true;
 		towerCluster.likeliness = 1;
 		towerCluster.description = "the tower";
@@ -24,7 +29,7 @@ public class Tower : MapAdjuster {
 	
 	public void drawTower(){
 		eraseTower();
-		towerCluster.tileType = TOWER;
+		towerCluster.tileType = towerType();
 		draw();
 	}
 
@@ -51,11 +56,23 @@ public class Tower : MapAdjuster {
 		IVector2 pos = new IVector2(this.transform.position.x,this.transform.position.y);
 		TileCluster t = towerCluster.copy();
 		for(int x = t.x; x < t.x + t.width; x++){
-			for(int y = t.y; y < t.y + t.height; y++){
+			for(int y = t.y; y < t.y + t.height + 2; y++){
 				if(Random.value < t.likeliness){
 					m.setTile(new IVector2(pos.x + x, pos.y + y),(byte)0,(byte)towerCluster.tileType);
 				}
 			}
 		}
+	}
+	
+	public int towerType(){
+		switch(type){
+		case TowerType.WOOD:
+			return TOWER_WOOD;
+		case TowerType.CLAY:
+			return TOWER_CLAY;
+		case TowerType.STONE:
+			return TOWER_STONE;
+		}
+		return TOWER_WOOD;
 	}
 }
