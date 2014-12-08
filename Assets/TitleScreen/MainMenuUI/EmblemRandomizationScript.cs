d0e37f4953a5befd64d0b82c773bfd31d7071edb
John Billingsley <jmb330@case.edu>
2014-12-08 13:52:58 -0500

    updated title menu UI

--- Assets/TitleScreen/MainMenuUI/EmblemRandomizationScript.cs
+++ Assets/TitleScreen/MainMenuUI/EmblemRandomizationScript.cs
@@ -1,20 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EmblemRandomizationScript : MonoBehaviour {
 
-	public Texture[] titles;
-	public float heightOffset;
-	public float scale;
+	public Image title;
+	public Sprite[] titles;
 	public int titleToUse;
-	public float ratio;
 	
 	void Start(){
+		if(title == null){
+			title = transform.GetComponent<Image>();
+		}
 		titleToUse = (int)Random.Range(0, titles.Length);
-		scale = scale / 100;
-	}
-	
-	void OnGUI(){
-		GUI.DrawTexture(new Rect(Screen.width/2 - titles[titleToUse].width/2*scale, Screen.height - Screen.height/heightOffset, titles[titleToUse].width*scale, titles[titleToUse].height*scale), titles[titleToUse]);
+		title.sprite = titles[titleToUse];
 	}
 }
