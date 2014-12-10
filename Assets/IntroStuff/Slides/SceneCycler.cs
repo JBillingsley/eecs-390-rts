560bfe4d70cd769c2b4c6eeb042ffb3e124adbb5
John <jmb330@case.edu>
2014-12-10 04:26:09 -0500

    implementing tutorial scene

---
+++ Assets/IntroStuff/Slides/SceneCycler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneCycler : MonoBehaviour {
+
+	public float cycleSpeed;
+	public Image[] scenes;
+	public string sceneToLoad;
+	public int count = 0;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < scenes.Length - 1; i++){
+			Invoke("cycleScene", cycleSpeed * (i +1));
+		}
+		Invoke("cycleScene", cycleSpeed * scenes.Length);
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(count == scenes.Length + 1){
+			Application.LoadLevel("TitleTest");
+		}
+	}
+	
+	public void cycleScene(){
+		
+		count = count + 1;
+		if(count >= scenes.Length){
+			if(sceneToLoad != null){
+				Application.LoadLevel("TitleTest");
+			}
+			return;
+		}
+		scenes[count-1].enabled = false;
+		toggleChildren(count -1, false);
+		scenes[count].enabled = true;
+		toggleChildren(count, true);
+	}
+	
+	public void toggleChildren(int number, bool enabled){
+		foreach(Image image in scenes[number].GetComponentsInChildren<Image>()){
+			image.enabled = enabled;
+		}
+	}
+}
