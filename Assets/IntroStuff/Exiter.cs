f731808155ffab7bf4a1b43f4f9a765c26fb379b
John <jmb330@case.edu>
2014-12-10 07:37:54 -0500

    added end scene and exit putton for main menus

---
+++ Assets/IntroStuff/Exiter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exiter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void exit(){
+		Application.Quit();
+	}
+}
